763809858ae92f7b0ba6102cf3826c7ed42c910d
samet <sametkayik@gmail.com>
2023-03-02 19:49:16 +0300

    Add auth package and route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -10,6 +11,9 @@ mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("Connected to database!"))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
