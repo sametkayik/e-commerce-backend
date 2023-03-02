de2172c686fbbf111ef0bf4fef672e5e63526acd
samet <sametkayik@gmail.com>
2023-03-02 20:06:11 +0300

    Add JWT to auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
@@ -22,18 +23,29 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    const hashedPassword = CryptoJS.AES.decrypt(
-      user.password,
-      process.env.SECRET_PASS
-    );
 
-    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-
-    if (originalPassword !== req.body.password) {
-      res.status(401).json("Wrong password!");
+    if (!user) {
+      res.status(401).json("Wrong Username");
     } else {
-      const { password, ...others } = user._doc;
-      res.status(200).json(others);
+      const hashedPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.SECRET_PASS
+      );
+
+      const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+      if (originalPassword !== req.body.password) {
+        res.status(401).json("Wrong password!");
+      } else {
+        const accessToken = jwt.sign(
+          { id: user._id, isAdmin: user.isAdmin },
+          process.env.JWT_PASS,
+          { expiresIn: "24h" }
+        );
+
+        const { password, ...others } = user._doc;
+        res.status(200).json({ ...others, accessToken });
+      }
     }
   } catch (err) {
     res.status(500).json(err);
