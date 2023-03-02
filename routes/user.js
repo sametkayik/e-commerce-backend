84a0af90b4e91f24d65d901f522f732eaf7fe3d2
samet <sametkayik@gmail.com>
2023-03-02 20:59:48 +0300

    Add User Search and Delete endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,9 @@
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuth } = require("./verifyToken");
+const {
+  verifyToken,
+  verifyTokenAndAdmin,
+  verifyTokenAndAuth,
+} = require("./verifyToken");
 
 const router = require("express").Router();
 
@@ -18,6 +22,37 @@ router.put("/:id", verifyTokenAndAuth, async (req, res) => {
       { new: true }
     );
     res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", verifyTokenAndAuth, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/search/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(10)
+      : await User.find();
+    res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
   }
