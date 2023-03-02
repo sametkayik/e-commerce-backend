b3007dc3c52422e15dbd1b653353c932b713d074
samet <sametkayik@gmail.com>
2023-03-02 20:46:02 +0300

    Add User and Admin Auth

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 
 dotenv.config();
@@ -14,6 +15,7 @@ mongoose
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
