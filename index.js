4b8ef4f22eeaf8e110e0993486e860191cd0d973
samet <sametkayik@gmail.com>
2023-03-02 18:44:52 +0300

    Add MongoDB connection

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to database!"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`);
 });
