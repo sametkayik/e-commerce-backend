92d21f03a014b990805e4e5371c8e9107068c745
samet <sametkayik@gmail.com>
2023-03-02 18:59:57 +0300

    Create Product  model

---
+++ models/Product.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    image: { type: String, required },
+    price: { type: Number, required: true },
+    size: { type: String },
+    color: { type: String },
+    categories: { type: Array },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
