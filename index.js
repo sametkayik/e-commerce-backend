75c8f53be09b0448b26281d47ab5fe7052a8981d
samet <sametkayik@gmail.com>
2023-03-08 15:06:43 +0300

    Add Product create/search/update endpoints

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
